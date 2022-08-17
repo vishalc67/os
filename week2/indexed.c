0e0b10acf4fd147a8a8db43424dcff77bd112c91
vishalc67 <107420423+vishalc67@users.noreply.github.com>
2022-08-17 21:39:50 +0530

    Create indexed.c

---
+++ week2/indexed.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int arr[50], temp[50], i, b, n, count, c = 1;
+    for (i = 0; i < 50; i++) {
+        arr[i] = 0;
+    }
+    while (c) {
+        a:
+        printf("enter index block:");
+        scanf("%d", &b);
+        if (arr[b] == 0){
+            printf("\nenter no.of blocks:");
+            scanf("%d", &n);
+            arr[b] = 1;
+        } else {
+            printf("\nblock is already allocated");
+            goto a;
+        }
+        b:
+        count = 0;
+        printf("\nEnter the blocks to be allocated:");
+        for (i = 0; i < n; i++) {
+            scanf("%d", &temp[i]);
+            if (arr[temp[i]] == 0)
+                count++;
+        }
+        if (count == n){
+            for (i = 0; i < n; i++)
+                arr[temp[i]] = 1;
+            printf("\nfile allocated");
+            printf("\tfile indexed");
+            for (i = 0; i < n; i++) {
+                printf("\n%d\t-->\t%d:1", b, temp[i], arr[temp[i]]);
+            }
+        } else {
+            printf("\nallocated");
+            printf("\nenter another file indexed");
+            goto b;
+        }
+        printf("\nEnter 0 to exit:");
+        scanf("%d", &c);
+    }
+    return 0;
+}
