c96e155b63ab0e5a5b12b4829f676f0586a7f23c
vishalc67 <107420423+vishalc67@users.noreply.github.com>
2022-08-17 22:05:33 +0530

    Create mvt.c

---
+++ week3/mvt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int partition[15],size,i=0,c=1;
+    printf("\nenter memory to be allocated:");
+    scanf("%d",&size);
+    while(c){
+        printf("\nenter process size:");
+        scanf("%d",&partition[i]);
+        if(size>partition[i]){
+            size=size-partition[i];
+            printf("remaining memory is %d",size);
+            i++;
+        }
+        else{
+            printf("\nrequired memory is not available");
+            break;
+        }
+        printf("\nenter 0 to exit");
+        scanf("%d",&c);
+        if(c==0)
+            printf("\n external fragmentation is %d",size);
+    }
+    return 0;
+}
