d09449de114362bf0bc87c6fd35f558c2c42e543
vishalc67 <107420423+vishalc67@users.noreply.github.com>
2022-08-17 20:32:35 +0530

    Create linked.c

---
+++ week2/linked.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main()
+{
+    int arr[50],a,i,n,c=1,b;
+    for(int i=0;i<50;i++)arr[i]=0;
+    printf("\nenter how many blocks already allocated:");
+    scanf("%d",&n);
+    printf("\nenter already allocated blocks:");
+    for(i=0;i<n;i++){
+        scanf("%d",&a);
+        arr[a]=1;
+    }
+    while(c){
+        printf("\nenter index starting block and length:");
+        scanf("%d %d",&b,&n);
+        if(arr[b]==0)
+            for(i=0;i<n;i++){
+                if(arr[b]!=1){
+                    arr[b]=1;
+                    printf("\n%d-->%d",b++,arr[b]);
+                }
+                else{
+                    printf("\nblock %d is already allocated",b);
+                    n++,b++;
+                }
+            }
+        else
+            printf("\nstarting block is already allocated");
+        printf("\nenter 0 to stop program:");
+        scanf("%d",&c);
+    }
+}
