31ab40ca805e4187172c6a79048dad310a81ed34
vishalc67 <107420423+vishalc67@users.noreply.github.com>
2022-08-21 17:54:33 +0530

    Update sjf.c

--- week1/sjf.c
+++ week1/sjf.c
@@ -1,46 +1,43 @@
 #include<stdio.h>
 struct sjf{
-    int bt,tat,wt,no,ct;
+    int wt,tat,bt,ct,no;
 }p[20];
-
-int main(){
-    int j,n,i;
-    printf("enter no of process");
+void main(){
+    int n;
+    printf("\nEnter number of processes:");
     scanf("%d",&n);
-    printf("enter burst times:\n");
-    for(i=0;i<n;i++){
+    printf("\nEnter burst times:");
+    for(int i=0;i<n;i++){
         scanf("%d",&p[i].bt);
-        p[i].no=i+1;
+        p[i].no = i+1;
     }
-    for(i=0;i<n-1;i++){
-        for(j=0;j<n-i-1;j++){
-            if (p[j].bt>p[j+1].bt){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(p[j].bt > p[j+1].bt){
                 struct sjf temp;
-                temp=p[j];
-                p[j]=p[j+1];
-                p[j+1]=temp;
+                temp = p[j];
+                p[j] = p[j+1];
+                p[j+1] = temp;
             }
         }
     }
-    p[0].tat=p[0].bt;
-    p[0].wt=0;
-    float avgwt=p[0].wt;
-    float avgtat=p[0].tat;
-    p[0].ct=p[0].bt;
-    for (i=1;i<n;i++){
-        p[i].ct=p[i-1].ct+p[i].bt;
-        p[i].tat=p[i].ct;
-        p[i].wt=p[i-1].ct;
-        avgtat+=p[i].tat;
-        avgwt+=p[i].wt;
+    p[0].wt = 0;
+    p[0].tat = p[0].bt;
+    p[0].ct = p[0].bt;
+    float AvgTat = p[0].tat;
+    float AvgWt = p[0].wt;
+    for(int i=1;i<n;i++){
+        p[i].ct = p[i-1].ct + p[i].bt;
+        p[i].tat = p[i].ct;
+        p[i].wt = p[i-1].ct;
+        AvgTat += p[i].tat;
+        AvgWt += p[i].wt;
     }
-    printf("------------------------------------------------------");
-    printf("\nProcess No Burst Time TAT\tWT\tCompletion Time\n");
-    printf("------------------------------------------------------\n");
-    for(i=0;i<n;i++)
-        printf("%d\t\t%d\t%d\t%d\t%d\n",p[i].no,p[i].bt,p[i].tat,p[i].wt,p[i].ct);
-    printf("-------------------------------------------------------\n");
-    printf("Avgwt:%f \t Avgtat:%f\n",avgwt/n,avgtat/n);
-    printf("-------------------------------------------------------\n");
-    return 0;
+    printf("\n--------------------------------------------------");
+    printf("\nProcess No Burst times TAT \t WT\t CT");
+    for(int i=0;i<n;i++)
+        printf("\n%d\t\t %d\t %d\t %d\t %d\t",p[i].no,p[i].bt,p[i].tat,p[i].wt,p[i].ct);
+    printf("\n----------------------------------------------------");
+    printf("\nAvgWt = %f \t AvgTat = %f",AvgWt/n,AvgTat/n);
+    printf("\n----------------------------------------------------");
 }
