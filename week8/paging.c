3606edfb8d642ca681cf3481f851575324e2efee
vishalc67 <107420423+vishalc67@users.noreply.github.com>
2022-08-21 23:12:30 +0530

    Update paging.c

--- week8/paging.c
+++ week8/paging.c
@@ -26,3 +26,86 @@ printf("\nPhysical address is:%d",phyadd);
 printf("\nDo you want to continue(1/0)?:");
 scanf("%d",&choice);
 }while(choice==1);
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+#include<stdio.h>
+void main(){
+    int page[50],i,np,nf,ps,off,pno,c = 1;
+    printf("\nEnter page size: ");
+    scanf("%d",&ps);
+    printf("\nEnter the no. of  pages: ");
+    scanf("%d",&np);
+    printf("\nEnter no. of frames: ");
+    scanf("%d",&nf);
+    for(i=0;i<np;i++)
+        page[i]=-1;
+    printf("\nEnter page table");
+    printf("\n(Enter frame no as -1 if that page is not present in any frame)");
+    printf("\npage no\tframe no");
+    for(i=0;i<np;i++){
+        printf("\n%d",i);
+        scanf("%d",&page[i]);
+    }
+    while(c){
+        printf("\nEnter the logical address(page no & offset):");
+        scanf("%d%d",&pno,&off);
+        if(page[pno]==-1)
+            printf("\nRequired page not available in frames");
+        else
+            printf("\n\nPhysical address is :%d",page[pno]+off);
+        printf("\nEnter 0 for exit:");
+        scanf("%d",&c);
+    }
+}
