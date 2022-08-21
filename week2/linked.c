6d316976b386e0d6efbc2764192645534e570cac
vishalc67 <107420423+vishalc67@users.noreply.github.com>
2022-08-21 20:50:46 +0530

    Update linked.c

--- week2/linked.c
+++ week2/linked.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
-void main()
-{
-    int arr[50],a,i,n,c=1,b;
-    for(int i=0;i<50;i++)arr[i]=0;
-    printf("\nenter how many blocks already allocated:");
-    scanf("%d",&n);
-    printf("\nenter already allocated blocks:");
-    for(i=0;i<n;i++){
-        scanf("%d",&a);
-        arr[a]=1;
-    }
-    while(c){
-        printf("\nenter index starting block and length:");
-        scanf("%d %d",&b,&n);
-        if(arr[b]==0)
-            for(i=0;i<n;i++){
-                if(arr[b]!=1){
-                    arr[b]=1;
-                    printf("\n%d-->%d",b++,arr[b]);
-                }
-                else{
-                    printf("\nblock %d is already allocated",b);
-                    n++,b++;
-                }
+
+void main() {
+    int arr[50], temp[50], i, b, n, count, c = 1;
+    for (i = 0; i < 50; i++) arr[i] = 0;
+    while (c) {
+        a:
+        printf("enter index block:");
+        scanf("%d", &b);
+        if (arr[b] == 0){
+            printf("\nenter no.of blocks:");
+            scanf("%d", &n);
+            arr[b] = 1;
+        } else {
+            printf("\nblock is already allocated");
+            goto a;
+        }
+        b:
+        count = 0;
+        printf("\nEnter the blocks to be allocated:");
+        for (i = 0; i < n; i++) {
+            scanf("%d", &temp[i]);
+            if (arr[temp[i]] == 0)
+                count++;
+        }
+        if (count == n){
+            for (i = 0; i < n; i++) {
+                arr[temp[i]] = 1;
+                printf("\n%d\t-->\t%d:1", b, temp[i], arr[temp[i]]);
             }
-        else
-            printf("\nstarting block is already allocated");
-        printf("\nenter 0 to stop program:");
-        scanf("%d",&c);
+            printf("\nfile allocated");
+            printf("\tfile indexed");
+        } else {
+            printf("\nNot allocated");
+            printf("\nEnter another file indexed");
+            goto b;
+        }
+        printf("\nEnter 0 to exit:");
+        scanf("%d", &c);
     }
 }
